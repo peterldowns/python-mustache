a7b66ef1b1089f3efc7aac8890242f8f97aaa50b
Peter Downs <peterldowns@gmail.com>
2013-03-04 10:36:24 -0800

    Bump version.

--- setup.py
+++ setup.py
@@ -7,14 +7,17 @@ setup(
     author = 'Peter Downs',
     author_email = 'peterldowns@gmail.com',
     url = 'https://github.com/peterldowns/python-mustache',
-    download_url = 'https://github.com/peterldowns/python-mustache/tarball/v0.1.0',
-    keywords = ['templating', 'template', 'mustache', 'web'],
+    download_url = 'https://github.com/peterldowns/python-mustache/tarball/v0.1.1',
+    keywords = [
+        'templating',
+        'template',
+        'mustache',
+        'web'],
     classifiers = [
         'Programming Language :: Python',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 2.7',
         'Topic :: Software Development :: Libraries :: Python Modules',
-        'Topic :: Text Processing :: Markup',
-    ],
+        'Topic :: Text Processing :: Markup'],
 )
