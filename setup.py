52fdcf0c008a37cefb8797c9738b7aa84bd6f6ee
Peter Downs <peterldowns@gmail.com>
2013-03-05 02:32:44 -0800

    Bump version.

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
     name = 'mustache',
     packages = ['mustache'],
-    version = '0.1.1',
+    version = '0.1.2',
     description = 'Mustache templating in Python',
     author = 'Peter Downs',
     author_email = 'peterldowns@gmail.com',
     url = 'https://github.com/peterldowns/python-mustache',
-    download_url = 'https://github.com/peterldowns/python-mustache/tarball/v0.1.1',
+    download_url = 'https://github.com/peterldowns/python-mustache/tarball/v0.1.2',
     keywords = [
         'templating',
         'template',
