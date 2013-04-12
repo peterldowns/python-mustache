30f3d9d17d060aa8cba16879289301fad909b543
Peter Downs <peterldowns@gmail.com>
2013-04-11 20:13:17 -0700

    Update and remove comments.

--- mustache/loading.py
+++ mustache/loading.py
@@ -1,6 +1,6 @@
 # coding: utf-8
-from os.path import (split, splitext, extsep, join, abspath, exists)
-from utils import (make_unicode)
+from os.path import split, splitext, extsep, join, abspath, exists
+from utils import make_unicode
 
 DEFAULT_EXTENSION = 'html'
 DEFAULT_DIRECTORY = 'static/templates'
@@ -8,18 +8,18 @@ DEFAULT_ENCODING = 'utf-8'
 DEFAULT_ERRORS = 'xmlcharrefreplace'
 
 def read(path):
-    """ Return the contents of a file as a byte string.  """
+    """ Return the contents of a file as a byte string. """
     with open(path, 'rb') as f:
         return f.read()
 
 def read_unicode(path, encoding, encoding_errors):
-    """ Return the contents of a file as a unicode string.  """
+    """ Return the contents of a file as a unicode string. """
     with open(path, 'rb') as f:
         return make_unicode(f.read(), encoding, encoding_errors)
 
 def get_abs_template_path(template_name, directory, extension):
-    """ Given a template name, a directory, and an extension,
-    return the absolute path to the template.  """
+    """ Given a template name, a directory, and an extension, return the
+    absolute path to the template. """
     # Get the relative path
     relative_path = join(directory, template_name)
     file_with_ext = template_name
@@ -28,7 +28,8 @@ def get_abs_template_path(template_name, directory, extension):
         # If there is a default extension, but no file extension, then add it
         file_name, file_ext = splitext(file_with_ext)
         if not file_ext:
-            file_with_ext = extsep.join((file_name, extension.replace(extsep, '')))
+            file_with_ext = extsep.join(
+                (file_name, extension.replace(extsep, '')))
             # Rebuild the relative path
             relative_path = join(directory, file_with_ext)
 
@@ -42,7 +43,7 @@ def load_file(path, encoding, encoding_errors):
     raise IOError("File {} does not exist".format(abs_path))
 
 def load_template(name, directory, extension, encoding, encoding_errors):
-    """ Load a template and return its contents as a unicode string.  """
+    """ Load a template and return its contents as a unicode string. """
     abs_path = get_abs_template_path(name, directory, extension)
     return load_file(abs_path, encoding, encoding_errors)
 
--- mustache/state.py
+++ mustache/state.py
@@ -1,26 +1,31 @@
 # coding: utf-8
 import re
-from os.path import (join, abspath)
+from os.path import join, abspath
 
-from copy import (copy)
-from stack import (Stack)
-from context import (Context)
+from copy import copy
+from stack import Stack
+from context import Context
 
 class State(object):
-    """ An object to keep track of the parser's state as it works its way 
+    """
+    An object to keep track of the parser's state as it works its way
     through a string to be parsed. Uses sane defaults (that comply with the
-    mustache spec), but allows for customization.  """
+    mustache spec), but allows for customization.
+    """
 
-    def __init__(self, partials_dir='.', extension='mustache', encoding='utf-8',
+    def __init__(self,
+                 partials_dir='.',
+                 extension='mustache',
+                 encoding='utf-8',
                  encoding_error='xmlcharrefreplace'):
         # Partials
         self.partials_dir = Stack(partials_dir)
         self.extension = extension
-        
+
         # Encoding
         self.encoding = encoding
         self.encoding_error = encoding_error
-        
+
         # State
         self.partials = Stack({})
         self.section = Stack()
@@ -39,52 +44,50 @@ class State(object):
             (?P<lead_wsp>[\ \t]*) # leading whitespace
             %(otag)s \s*
             (?:
-                (?P<change>=) \s* (?P<delims>.+?)   \s* = |             # change delimiters OR 
-                (?P<raw>{)    \s* (?P<raw_key>.+?) \s* } |                # raw format (no escaping) OR
-                (?P<tag_type>[%(types)s]?)  \s* (?P<tag_key>[\s\S]+?)    # other tag types
+                (?P<change>=) \s* (?P<delims>.+?)   \s* = |           # change delimiters OR
+                (?P<raw>{)    \s* (?P<raw_key>.+?) \s* } |            # raw format (no escaping) OR
+                (?P<tag_type>[%(types)s]?)  \s* (?P<tag_key>[\s\S]+?) # other tag types
             )
             \s* %(ctag)s (?P<end_wsp>[\ \t]*)"""
         self.re_flags = re.MULTILINE|re.VERBOSE|re.DOTALL|re.UNICODE
         self.tag_re = None # replaced by update_tag_re
         self.update_tag_re()
 
-    
+
     def compile_tag_re(self, tags):
         """
-        Return the regex used to look for Mustache tags compiled
-        to work with specific opening tags, close tags, and tag
-        types.
+        Return the regex used to look for Mustache tags compiled to work with
+        specific opening tags, close tags, and tag types.
         """
         return re.compile(self.raw_tag_re % tags, self.re_flags)
 
     def update_tag_re(self):
         """
-        Update the compiled tag regex to use the tags on the top
-        of the tag stack. This should be called any time the tag
-        stack is changed.
+        Update the compiled tag regex to use the tags on the top of the tag
+        stack. This should be called any time the tag stack is changed.
         """
         self.tag_re = self.compile_tag_re(self.tags())
 
     def push_tags(self, tags):
         """
-        Pushes the new tags to the top of the tag state and updates
-        the compiled tag regex.
+        Pushes the new tags to the top of the tag state and updates the
+        compiled tag regex.
         """
         self.tags.push(tags)
         self.update_tag_re()
 
     def pop_tags(self):
         """
-        Pops the current tags from the top of the tag state and
-        updates the compiled tag regex.
+        Pops the current tags from the top of the tag state and updates the
+        compiled tag regex.
         """
         self.tags.pop()
         self.update_tag_re()
-    
+
     @property
     def abs_partials_dir(self):
         """
-        Returns the absolute path to the current partials directory
-        based on the current stack of relative partial directories.
+        Returns the absolute path to the current partials directory based on
+        the current stack of relative partial directories.
         """
         return abspath(join(*self.partials_dir))
